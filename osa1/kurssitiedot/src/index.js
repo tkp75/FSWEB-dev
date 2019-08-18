0b9f81a08f9a53aa9f0671fcac45382b2b6a3037
tkp75 <tommi.pakarinen@gmail.com>
2019-08-18 14:33:52 +0300

    1.4 defined parts as an array

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -19,46 +19,45 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+     {props.parts.map((part,index) => <Part key={index} part={part} />)}
     </>
   )
 }
 
 // Total
-const Total = (props) => {
+const Total = (props) => { 
   return (
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {props.parts.map(part => part.exercises).reduce((a,b) => a + b, 0)}</p>
   )
 }
 
 // App
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header
         course={course}
       />
       <Content
-        part1={part1} part2={part2} part3={part3}
+        parts={parts}
       />
       <Total
-        total={part1.exercises + part2.exercises + part3.exercises}
+        parts={parts}
       />
     </div>
   )
