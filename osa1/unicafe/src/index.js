c1a7bd91ffbe3d7007ec65d035c3bf5d60c3c7e1
tkp75 <tommi.pakarinen@gmail.com>
2019-08-18 23:32:50 +0300

    1.6 unicafe step1 tested

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,6 +1,17 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({text}) => <h1>{text}</h1>
+
+const Button = ({handleClick, text}) => <button onClick={handleClick} >{text}</button>
+
+const Statistics = ({good, neutral, bad}) =>
+  <ul style={{listStyleType: "none", padding: 0}}>
+    <li>good {good}</li>
+    <li>neutral {neutral}</li>
+    <li>bad {bad}</li>
+  </ul>
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -9,7 +20,12 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header text='give feedback' />
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Header text='statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
