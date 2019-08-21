82b2019f5a2a9fe13cc1997a02daeb95209580f3
tkp75 <tommi.pakarinen@gmail.com>
2019-08-21 23:10:31 +0300

    2.2: output total in bold

--- osa2/kurssitiedot/src/components/Total.js
+++ osa2/kurssitiedot/src/components/Total.js
@@ -4,7 +4,8 @@ import React from 'react'
 const Total = (props) => { 
   //console.log('Total: ',props)
   return (
-      <p>Number of exercises {props.parts.map(part => part.exercises).reduce((a,b) => a + b, 0)}</p>
+      <p><b>total of {props.parts.map(part => part.exercises).reduce((a,b) => a + b, 0)} exercises</b></p>
+
   )
 }
 
