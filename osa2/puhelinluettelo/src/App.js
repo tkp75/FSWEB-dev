ce57300caf03dc56f912d5fdebb6b33da52919db
tkp75 <tommi.pakarinen@gmail.com>
2019-08-22 23:28:16 +0300

    2.6 puhelinluettelo: handlers added for form and numbers displayed

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,19 +6,40 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleInputChange = (event) => setNewName(event.target.value)
+  
+  const handleSubmitClick = (event) => {
+    event.preventDefault()
+    if(persons.find(person => person.name === newName) === undefined) {
+      setPersons(persons.concat({ name: newName }))
+    }
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name:
+          <input
+            onChange={handleInputChange}
+            value={newName}
+          />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button
+            type="submit"
+            onClick={handleSubmitClick}
+          >
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul style={{listStyleType: "none", padding: 0}}>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
     </div>
   )
 
