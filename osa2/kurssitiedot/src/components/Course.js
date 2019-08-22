f5fca4252e1036540cea043b95c359e04400def9
tkp75 <tommi.pakarinen@gmail.com>
2019-08-22 22:12:13 +0300

    2.5 kurssitiedot: use part id as a key

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -15,7 +15,7 @@ const Content = (props) => {
   //console.log('Content: ',props)
   return (
     <>
-     {props.parts.map((part,index) => <Part key={index} part={part} />)}
+     {props.parts.map(part => <Part key={part.id} part={part} />)}
     </>
   )
 }
