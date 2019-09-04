7a5d0590bbe05b2bb24e52a53f05ec1f28fe0136
tkp75 <tommi.pakarinen@gmail.com>
2019-09-04 20:45:43 +0300

    3.17: added modify method (HTTP PUT) for person service

--- osa3/puhelinluettelo-frontend/src/services/persons.js
+++ osa3/puhelinluettelo-frontend/src/services/persons.js
@@ -11,9 +11,14 @@ const create = (newPerson) => {
   return request.then(response => response.data)
 }
 
+const modify = (newPerson) => {
+  const request = axios.put(baseUrl, newPerson)
+  return request.then(response => response.data)
+}
+
 const remove = (id) => {
   const request = axios.delete(`${baseUrl}/${id}`)
   return request.then(response => response.data)
 }
 
-export default { getAll, create, remove }
+export default { getAll, create, modify, remove }
