2d4521472265a4e3062a588733996c2c263364d8
tkp75 <tommi.pakarinen@gmail.com>
2019-09-14 23:08:27 +0300

    4.8: middleware and logger added

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,28 +1,30 @@
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const blogsRouter = require('./controllers/blogs')
-//const middleware = require('./utils/middleware')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 const app = express()
 
-console.log('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.set('useCreateIndex', true)
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error while connecting to MongoDB:', error.message)
+    logger.error('error while connecting to MongoDB:', error.message)
   })
 
 app.use(cors())
 //app.use(express.static('build'))
 app.use(bodyParser.json())
-//app.use(middleware.requestLogger)
+app.use(middleware.requestLogger)
 app.use('/api/blogs',blogsRouter)
-//app.use(middleware.unknownEndpoint)
-//app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
