ddc3ec15d0b59779cd4c5dcddeed9264af1b097c
tkp75 <tommi.pakarinen@gmail.com>
2019-09-14 23:06:06 +0300

    4.8: separate DBs for DEV/TEST/PROD

--- osa4/blogilista/utils/config.js
+++ osa4/blogilista/utils/config.js
@@ -1,7 +1,17 @@
 require('dotenv').config()
 
+// Choose MONGODB_URI based on development cycle
+let DB_URI
+if (process.env.NODE_ENV === 'development') {
+  DB_URI = process.env.DEV_MONGODB_URI
+} else if (process.env.NODE_ENV === 'test') {
+  DB_URI = process.env.TEST_MONGODB_URI
+} else {
+  DB_URI = process.env.MONGODB_URI
+}
+
+const MONGODB_URI = DB_URI
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
