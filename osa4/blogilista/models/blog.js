90f25a25c6436c1a4fa3d8be1e033d89205f5745
tkp75 <tommi.pakarinen@gmail.com>
2019-09-14 23:10:58 +0300

    4.8: added basic validation for schema

--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+  title:  { type: String, required: true },
+  author: { type: String, required: true },
+  url:    { type: String, unique:   true },
+  likes:  { type: Number }
 })
+blogSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Blog', blogSchema)
