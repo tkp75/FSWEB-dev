0b1890bbcae7d0ec9ec12e34115cf4e3b489b145
tkp75 <tommi.pakarinen@gmail.com>
2019-09-15 18:32:04 +0300

    4.11:  likes set to 0 if missing

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -3,9 +3,9 @@ const supertest = require('supertest')
 const Blog = require('../models/blog')
 const app = require('../app')
 const helper = require('./test_helper')
+//const util = require('util')
 
 const api = supertest(app)
-
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -70,7 +70,7 @@ describe('getBlogs', () => {
 
 describe('setBlogs', () => {
 
-  test('a valid blog can be added ', async () => {
+  test('a valid blog can be added', async () => {
     const newBlog = {
       'title': 'Blog Title',
       'author': 'Blog Author',
@@ -88,7 +88,7 @@ describe('setBlogs', () => {
     expect(titles).toContain('Blog Title')
   })
 
-  test('blog only with likes is not added', async () => {
+  test('blog with likes only is not added', async () => {
     const newBlog = {
       likes: 0
     }
@@ -99,6 +99,27 @@ describe('setBlogs', () => {
     const blogsAtEnd = await helper.blogsInDb()
     expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
   })
+
+  test('blog without likes is added', async () => {
+    const newBlog = {
+      'title': 'Blog Title',
+      'author': 'Blog Author',
+      'url': 'http://inter.net/blog?id=1',
+    }
+    const result = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    //console.log(util.inspect(result.body))
+    expect(result.body).toHaveProperty('likes',0)
+    expect(result.body.id).toBeDefined()
+    const blogsAtEnd = await helper.blogsInDb()
+    const blog = blogsAtEnd.filter(blog => blog.id === result.body.id)[0]
+    expect(blog).toHaveProperty('likes',0)
+    expect(blog).toHaveProperty('title',newBlog.title)
+  })
+
 })
 
 /* API functions not implemented yet
