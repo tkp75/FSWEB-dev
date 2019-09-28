48d45276bd12824027b9482e8f566da7b8fbb057
tkp75 <tommi.pakarinen@gmail.com>
2019-09-28 19:01:49 +0300

    5.1 user's name added to blogs page

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -81,6 +81,7 @@ function App() {
   return (
     <div className="App">
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
