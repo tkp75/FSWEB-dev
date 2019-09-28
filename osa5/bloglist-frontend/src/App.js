c06d2b3c2adc3c9827aa469803ddf32652c500ef
tkp75 <tommi.pakarinen@gmail.com>
2019-09-28 18:50:47 +0300

    5.1: first working version

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,35 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import './App.css';
 import Blog from './components/Blog';
-const blogs = []
-const user = null
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const LoginForm = (props) => {
+  return (
+    <form>
+      <div>
+        username<input type="text" onChange={props.changeUsernameHandler} name="Username" value={props.username}/>
+      </div>
+      <div>
+        password<input type="password" onChange={props.changePasswordHandler} name="Password" value={props.password}/>
+      </div>
+      <div>
+        <button type="submit" onClick={props.loginClickHandler} name="Login">
+          login
+        </button>
+      </div>
+    </form>
+  )
+}
+
 
 function App() {
-  if (user === null) {
+  const [ username, setUsername ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const [ user, setUser ] = useState(null)
+  const [ blogs, setBlogs ] = useState([])
+
+  useEffect(() => {
+    blogService
+      .getAll().then(initialNotes => {
+        setBlogs(initialNotes)
+      })
+  }, [])
+
+  const handleUsernameChange = (event) => setUsername(event.target.value)
+  const handlePasswordChange = (event) => setPassword(event.target.value)
+  const handleLoginClick = async (event) => {
+    event.preventDefault()
+    if (!username || !password) {
+      alert(`Error, please fill in both username and password`)
+      setUser(null)
+      return
+    }  
+    try {
+      const loginResponse = await loginService.login({ username, password })
+      if (!loginResponse || !loginResponse.token) {
+        console.log('Login failed: loginResponse=',loginResponse)
+        alert(`Error, login failed\n${loginResponse}`)
+        setUser(null)
+        return
+      }
+      setUser(loginResponse)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      alert(`Error, login failed\n${exception}`)
+    }
+  } 
+  
+  if (!user) {
+    // Show login form if not logged in
     return (
       <div className="App">
         <h2>Log in to application</h2>
-        <form>
-          <div>
-            username
-            <input/>
-          </div>
-          <div>
-            password
-            <input/>
-          </div>
-          <div>
-            <button>
-              login
-            </button>
-          </div>
-        </form>
+        <LoginForm
+          changeUsernameHandler={handleUsernameChange}
+          username={username}
+          changePasswordHandler={handlePasswordChange}
+          password={password}
+          loginClickHandler={handleLoginClick}
+        />
       </div>
     )
   }
 
+  // Show blogs if logged in
   return (
-    //Blog({ title: "foo", author: "bar" })
     <div className="App">
       <h2>blogs</h2>
       {blogs.map(blog =>
