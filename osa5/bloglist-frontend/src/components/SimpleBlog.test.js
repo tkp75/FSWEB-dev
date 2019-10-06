5f89e366dea5a4cfb27d8550a6d636373dba6386
Pakarinen <tommi.pakarinen@nokia.com>
2019-10-06 19:38:38 +0300

    5.14: test like button on SimpleBlog

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
 const blog = {
@@ -9,16 +9,16 @@ const blog = {
   likes: 1
 }
 
+const mockHandler = jest.fn()
+
 afterEach(cleanup)
 
 test('renders title', () => {
-  const mockHandler = jest.fn()
   const component = render(<SimpleBlog blog={blog} onClick={mockHandler}/>)
   expect(component.container).toHaveTextContent('Blog Title')
 })
 
 test('renders author', () => {
-  const mockHandler = jest.fn()
   const component = render(<SimpleBlog blog={blog} onClick={mockHandler}/>)
   const element = component.getByText(/.*Blog Author/)
   expect(element).toBeDefined()
@@ -27,7 +27,14 @@ test('renders author', () => {
 test('renders likes', () => {
   const mockHandler = jest.fn()
   const component = render(<SimpleBlog blog={blog} onClick={mockHandler}/>)
-
   const div = component.container.querySelector('.simpleblog')
   expect(div).toHaveTextContent(/blog has [0-9]+ likes/)
+})
+
+test('like button is clicked twice', () => {
+  const component = render(<SimpleBlog blog={blog} onClick={mockHandler}/>)
+  const button = component.getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+  expect(mockHandler.mock.calls.length).toBe(2)
 })
