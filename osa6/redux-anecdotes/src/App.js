d0b63ea9b4bfacc469d8ccfa1c75e97e1a8a861e
tkp75 <tommi.pakarinen@gmail.com>
2019-10-16 22:06:03 +0300

    6.9 redux-anecdotes: notificationReducerin created

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import Notification from './components/Notification'
 
 const App = (props) => {
   return (
@@ -8,6 +9,7 @@ const App = (props) => {
       <h2>Anecdotes</h2>
       <AnecdoteList store={props.store} />
       <AnecdoteForm store={props.store} />
+      <Notification store={props.store} />
     </div>
   )
 }
