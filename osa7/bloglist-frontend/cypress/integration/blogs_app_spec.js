4c7cb53b639e3d296c3f89970191bf932ae4db69
tkp75 <tommi.pakarinen@gmail.com>
2020-01-04 21:47:33 +0200

    7.18 Frontend tests cleanup DB and create user

--- osa7/bloglist-frontend/cypress/integration/blogs_app_spec.js
+++ osa7/bloglist-frontend/cypress/integration/blogs_app_spec.js
@@ -1,25 +1,43 @@
 /* eslint-disable no-undef */
 describe('Blogs ', function() {
+
+  // Cleanup database and create user
   beforeEach(function() {
-    cy.visit('/logout')
-    cy.get('h2').contains('blogs')
-    cy.get('h3').contains('Log in')
-    cy.contains('username')
-    cy.contains('password')
-    cy.get(':nth-child(1) > input').type('D')
-    cy.get(':nth-child(2) > input').type('iso')
-    cy.get('.form > .ui').contains('login').click()
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Test User',
+      username: 'tu',
+      password: 'cryptic'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
   })
 
-  it('User is logged in', function() {
-    cy.contains('logout')
-    cy.contains('D logged in')
-  })
+  describe('when logged in', function() {
 
-  it('Home page can be opened', function() {
-    cy.get('.inverted > :nth-child(1) > a').contains('home')
-    cy.get('h2').contains('blogs')
-    cy.contains('new blog')
-  })
+    // Logout and login
+    beforeEach(function() {
+      cy.visit('/logout')
+      cy.get('h2').contains('blogs')
+      cy.get('h3').contains('Log in')
+      cy.contains('username')
+      cy.contains('password')
+      cy.get(':nth-child(1) > input').type('tu')
+      cy.get(':nth-child(2) > input').type('cryptic')
+      cy.get('.form > .ui').contains('login').click()
+    })
 
+    // Test if login worked
+    it('User is logged in', function() {
+      cy.contains('logout')
+      cy.contains('tu logged in')
+    })
+
+    // Test that home page was opened after the login
+    it('Home page can be opened', function() {
+      cy.get('.inverted > :nth-child(1) > a').contains('home')
+      cy.get('h2').contains('blogs')
+      cy.contains('new blog')
+    })
+
+  })
 })
